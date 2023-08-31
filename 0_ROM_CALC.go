5af925d0a222af4f419e5c925870f2071a6506ba
n1ce2meet <96385722+n1ce2meet@users.noreply.github.com>
2023-08-31 03:18:01 +0300

    Add files via upload

--- 0_ROM_CALC.go
+++ 0_ROM_CALC.go
@@ -8,157 +8,153 @@ import (
 	"strings"
 )
 
-var ROM2ARABIC map[string]int = map[string]int{
-	"nulla": 0, "I": 1, "II": 2, "III": 3, "IV": 4, "V": 5, "VI": 6, "VII": 7, "VIII": 8, "IX": 9, "X": 10, "XI": 11,
-	"XII": 12, "XIII": 13, "XIV": 14, "XV": 15, "XVI": 16, "XVII": 17, "XVIII": 18, "XIX": 19, "XX": 20,
+var ROMAN2ARABIC map[string]int = map[string]int{
+	"C": 100, "XC": 90, "L": 50, "XL": 40, "X": 10, "IX": 9, "V": 5, "IV": 4, "I": 1,
 }
 
-var ARABIC2ROME map[int]string = map[int]string{
-	0: "nulla", 1: "I", 2: "II", 3: "III", 4: "IV", 5: "V", 6: "VI", 7: "VII", 8: "VIII", 9: "IX", 10: "X",
-	11: "XI", 12: "XII", 13: "XIII", 14: "XIV", 15: "XV", 16: "XVI", 17: "XVII", 18: "XVIII", 19: "XIX", 20: "XX",
-}
+var ROMAN2ARABICMAPORDER []string = []string{"C", "XC", "L", "XL", "X", "IX", "V", "IV", "I"} // к сожалению привычный ordermap в стандарте отсутсвует
 
-var OPERATORS []string = []string{
-	"+", "-", "*", "/",
-}
-
-func contains(val string, arr []string) bool {
-	result := false
-
-	for _, value := range arr {
-		if val == value {
-			result = true
-			break
-		}
-	}
-
-	return result
-}
+var OPERATORS string = "+-/*"
 
 func isOperator(operator string) bool {
 	result := false
-	if contains(operator, OPERATORS) {
+	if strings.Contains(OPERATORS, operator) {
 		result = true
 	}
 	return result
 }
 
-func Convert2RomNumber(number int) string {
-	return ARABIC2ROME[number]
-}
-
-func Convert2ArabicNumber(romNum string) int {
-	return ROM2ARABIC[romNum]
-}
-
-func isRomeNumber(number string) bool {
+func isRomanNumeral(number string) bool {
 	result := true
-	_, ok := ROM2ARABIC[number]
+	_, ok := ROMAN2ARABIC[number]
 	if !ok {
 		result = false
 	}
 	return result
 }
 
-func isArabicNumber(number string) bool {
-	result := true
-	numberLI, err := strconv.Atoi(number)
-	if err != nil {
-		panic(fmt.Sprintf("Expression entered incorrectly: number %v entered incorrectly", number))
+func toRoman(number int) string {
+	result := ""
+
+	if number == 0 {
+		result = "nulla"
 	} else {
-		_, ok := ARABIC2ROME[numberLI]
-		if !ok {
-			result = false
+		for _, roman := range ROMAN2ARABICMAPORDER[:] {
+			arabic := ROMAN2ARABIC[roman]
+			result += strings.Repeat(roman, (number / arabic))
+			number %= arabic
 		}
 	}
+
 	return result
 }
 
-func MakeCalc(val1 int, val2 int, op string) int {
+func toArabic(number string) int {
 	result := 0
+	byNumeral := strings.Split(number, "")
 
-	if val1 < 1 || val2 < 1 {
-		panic("Expression entered incorrectly: entered number is too small")
-	}
+	for i := 0; i < len(byNumeral); i++ {
+		if isRomanNumeral(byNumeral[i]) {
 
-	if val1 > 10 || val2 > 10 {
-		panic("Expression entered incorrectly: entered number is too big")
-	}
+			if i+1 < len(number) {
+				if ROMAN2ARABIC[byNumeral[i]] < ROMAN2ARABIC[byNumeral[i+1]] {
+					result += ROMAN2ARABIC[byNumeral[i]+byNumeral[i+1]]
+					i++
+					continue
+				}
+			}
 
-	switch op {
-	case "+":
-		result = val1 + val2
-	case "-":
-		result = val1 - val2
-	case "*":
-		result = val1 * val2
-	case "/":
-		result = val1 / val2
+			result += ROMAN2ARABIC[byNumeral[i]]
+
+		} else {
+			panic(fmt.Sprintf("Expression entered incorrectly: %v is not roman number", number))
+		}
 	}
 
 	return result
 }
 
-func CheckInputForNumberOfElements(input []string) {
+func CheckExp(input []string) {
 
 	if len(input) != 3 {
 		panic("Expression entered incorrectly: wrong number of arguments.")
 	}
 
-}
-
-func ExpressionCheck(operator string, leftOperand string, rightOperand string, pred func(string) bool) {
-
-	if !isOperator(operator) {
-		panic(fmt.Sprintf("Expression entered incorrectly: operator %v entered incorrectly", operator))
-	}
-	if !pred(leftOperand) {
-		panic(fmt.Sprintf("Expression entered incorrectly: number %v entered incorrectly", leftOperand))
-	}
-	if !pred(rightOperand) {
-		panic(fmt.Sprintf("Expression entered incorrectly: number %v entered incorrectly", rightOperand))
+	if !isOperator(input[1]) {
+		panic("Expression entered incorrectly: operator entered incorrectly.")
 	}
 
 }
 
-func CalcRome(left string, right string, operator string) string {
+func CheckEnteredNumbers(numbers ...int) {
+	for _, num := range numbers[:] {
+		if num < 1 {
+			panic(fmt.Sprintf("Expression entered incorrectly: number %v is too small", num))
+		}
+		if num > 10 {
+			panic(fmt.Sprintf("Expression entered incorrectly: number %v is too big", num))
+		}
+	}
+}
+
+func MakeCalc(left int, right int, op string) int {
 	result := 0
-	ExpressionCheck(operator, left, right, isRomeNumber)
-	result = MakeCalc(Convert2ArabicNumber(left), Convert2ArabicNumber(right), operator)
+
+	CheckEnteredNumbers(left, right)
+
+	switch op {
+	case "+":
+		result = left + right
+	case "-":
+		result = left - right
+	case "*":
+		result = left * right
+	case "/":
+		result = left / right
+	}
+
+	return result
+}
+
+func CalcRoman(left string, right string, operator string) string {
+	result := 0
+	result = MakeCalc(toArabic(left), toArabic(right), operator)
 	if result < 0 {
 		panic("The result of expression is a number less than zero. In Roman calculus there are no numbers less than zero.")
 	}
-	return Convert2RomNumber(result)
+	return toRoman(result)
 }
 
 func CalcArabic(left string, right string, operator string) string {
 	result := 0
-	ExpressionCheck(operator, left, right, isArabicNumber)
-	l, _ := strconv.Atoi(left)
-	r, _ := strconv.Atoi(right)
+	l, lerr := strconv.Atoi(left)
+	r, rerr := strconv.Atoi(right)
+	if lerr != nil {
+		panic(fmt.Sprintf("Expression entered incorrectly: number %v is incorrect", left))
+	} else if rerr != nil {
+		panic(fmt.Sprintf("Expression entered incorrectly: number %v is incorrect", right))
+	}
 	result = MakeCalc(l, r, operator)
 	return strconv.Itoa(result)
 }
 
 func Calc(input []string) string {
 
-	CheckInputForNumberOfElements(input)
+	CheckExp(input)
+
 	operator := input[1]
 	leftOperand := input[0]
 	rightOperand := input[2]
 
 	var result string
 
-	if isRomeNumber(leftOperand) {
-		result = CalcRome(leftOperand, rightOperand, operator)
+	if isRomanNumeral(string(leftOperand[0])) {
+		result = CalcRoman(leftOperand, rightOperand, operator)
 	} else {
 		result = CalcArabic(leftOperand, rightOperand, operator)
 	}
 
 	return result
-}
-
-func SafeCalc() {
 }
 
 func main() { // не успел разобраться с обработкой ошибок в го в связи с чем закрываю программу через панику что не очень хорошо
